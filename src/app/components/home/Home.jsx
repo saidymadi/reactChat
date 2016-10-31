067bc8cf09545390dee35dc457e488fddee9f5cf
PROD\smadi <said.madi@software.dell.com>
2016-10-31 16:02:26 -0400

    code styling

--- src/app/components/home/Home.jsx
+++ src/app/components/home/Home.jsx
@@ -165,13 +165,13 @@ export default class Home extends Component {
   }
 
  generateNewGUID() {
-            function s4() {
-                return Math.floor((1 + Math.random()) * 0x10000)
-                    .toString(16)
-                    .substring(1);
-            }
-            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-                s4() + '-' + s4() + s4() + s4();
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
   }
 
 
