459a3ceb6826cd6103734b8005e4f65c3acf1e59
PROD\smadi <said.madi@software.dell.com>
2016-10-31 04:11:21 -0400

    improvement to layour and logic

--- src/app/components/home/Home.jsx
+++ src/app/components/home/Home.jsx
@@ -46,6 +46,9 @@ export default class Home extends Component {
  }
 
  handleAddUser() {
+  //do some validation 
+  //make sure no duplicate users sanitize the string from any chars that are not allowed 
+
 
   const { socket, users, dispatch} = this.props;     
   let newUser = { name : this.state.userNameTextField , id: this.generateNewGUID()}; 
@@ -53,6 +56,12 @@ export default class Home extends Component {
   dispatch(actions.addUser(newUser));
   dispatch(actions.selectUser(newUser));
   socket.emit('addUser', newUser);
+
+  //clear state and field vals
+   this.setState({ userNameTextField: "",
+                    userBtnStatus: false
+                 });
+     
 
  }
  handleUserSelection(user){
@@ -70,6 +79,7 @@ export default class Home extends Component {
   this.setState({ userNameTextField: fieldVal,
                     userBtnStatus: enableButton
                  });
+
     
  }
 
@@ -77,7 +87,7 @@ export default class Home extends Component {
 
     var listItems = this.props.users.map((item) => {
   
-          debugger;
+
         if(this.props.selectedUser.id != item.id){
           return (<li style={{padding:"0px" , marginBottom:"6px" , border:"none"}} className="list-group-item animated fadeIn" key={item.id}>
              <button   onClick={()=> this.handleUserSelection(item)}  style={{width: "100%"}} className="btn btn-secondary">  {item.name}</button>
@@ -96,22 +106,23 @@ export default class Home extends Component {
     return (
         <div className="container-fluid">
            <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-3">
             <div className="panel panel-primary">
-                 <div className="panel-heading">
+                 <div  className="panel-heading">
                    <h3 className="panel-title">Users Panel</h3>
                   </div>
-                 <div style={{height: "calc(100vh - 250px)"}} className="panel-body">
-                    <div className="row">
-                      <div className=" col-lg-12">
-                      <div className="input-group">
-                        <input onChange={(evt)=> this.handleUserTextFieldChange(evt)} type="text" className="form-control" placeholder="type in a user name"/>
-                        <span className="input-group-btn">  
-                          <button disabled={!this.state.userBtnStatus} onClick={(evt)=> this.handleAddUser(evt)} type="button" className="btn btn-primary">Add</button>
-                        </span>
-                      </div>
-                      </div>
+                   <div style={{padding:"15px", paddingBottom: "0px"}} className="row">
+                    <div className=" col-lg-12">
+                    <div className="input-group">
+                      <input onChange={(evt)=> this.handleUserTextFieldChange(evt)} value={this.state.userNameTextField} type="text" className="form-control" placeholder="type in a user name"/>
+                      <span className="input-group-btn">  
+                        <button disabled={!this.state.userBtnStatus} onClick={(evt)=> this.handleAddUser(evt)} type="button" className="btn btn-primary">Add</button>
+                      </span>
                     </div>
+                    </div>
+                  </div>
+                 <div style={{height: "calc(100vh - 200px)" , overflowY:"auto", paddingTop: "0px"}} className="panel-body">
+                 
                     <div style={{color:"grey" , fontWeight:"700", marginTop:"15px", borderBottom:"1px solid lightgrey "}} ><h3 className="panel-title">Select a user and chat on their behalf</h3></div>
                     <div style={{marginTop:"10px"}}>
                        <ul className="list-group">
@@ -123,12 +134,12 @@ export default class Home extends Component {
               </div>
           </div>
 
-          <div className="col-md-6">
+          <div className="col-md-9">
           <div className="panel panel-primary">
                   <div className="panel-heading">
                       <h3 className="panel-title">Chat Stream</h3>
                   </div>
-                  <div style={{height: "calc(100vh - 250px)"}}  className="panel-body">
+                  <div style={{height: "calc(100vh - 250px)" , overflowY:"auto"}}  className="panel-body">
                       Panel content
                   </div>
                    <div style={{height: "110px"}} id="chat-panel-footer" className="panel-footer">
